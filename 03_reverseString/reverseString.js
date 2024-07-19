04d7e8334cf02543f4cf042fbaad49db9eb7e209
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-19 07:14:21 -0700

    Create reverseString function
    Reverses a string by extracting out each character by for loop,
    then adding it to the front of an already existing string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    for (let i=0; i < string.length; i++) {
+        let char = string.charAt(i);
+        newString = char + newString;
+    }
+    return newString;
 };
 
 // Do not edit below this line
