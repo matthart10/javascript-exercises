4126e8b20e43f27e688b9726d33b6b2187e8e9ba
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-19 07:16:02 -0700

    Create function to repeat a starting
    Create function that takes in a string and number, number determines
    how many times a string is repeated. Use a for loop to concatenate
    string onto a string.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let strings = "";
+    for (let i = 0; i < num; i++) {
+        strings += string;
+    }
+return strings;
 };
 
 // Do not edit below this line
