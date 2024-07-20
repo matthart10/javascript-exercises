2f49684cded95ff7e1ceea2cdb26dc37421f41a7
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-20 09:27:53 -0700

    Create function that removes elements from array
    Create a new array, and use for loop that loops through all instances
    of arguments that are present. If statement that if there are
    arguments that aren't in the array, then move to the next iteration.
    If there is, then filter out all instances of that argument in the
    array and return the new array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...allArgs) {
+    let newArray = array;
+    for (let arg of allArgs) {
+        if (newArray.includes(arg)) {
+            newArray = newArray.filter(function(filteredArray) {
+                return filteredArray != arg;
+            })
+        } else {
+            continue;
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
