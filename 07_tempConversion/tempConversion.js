a81c93c5cd9e5ee01f0f9bb7d4a1ffce4612b416
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-21 09:15:20 -0700

    Create function that converts temperatures
    Use Math.round to round to the nearest tenths place if necessary,
    simple equation to convert numbers from each other.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let degrees = (num - 32) / (9/5)
+  return Math.round(degrees * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let degrees = num * (9/5) + 32;
+  return Math.round(degrees * 10) / 10;
 };
 
 // Do not edit below this line
