b6bd73ad75c39012e65c99344ea59c8272390d14
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-08-12 18:25:47 -0700

    Create functions that does math
    Create add, subtract, sum, multiply, power and factorial
    functions and test them using tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let product = 1;
+  for (i = 1; i < a+1; i++) {
+    product = product * i;
+  };
+  return product;
 };
 
 // Do not edit below this line
