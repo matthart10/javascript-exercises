84ecf2b77f504a48506064d87e361b7a65af7735
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-08-14 06:05:21 -0700

    Use map to output array of titles in array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let booksArray = arr.map(item => item.title);
+    return booksArray;
+    
 };
 
 // Do not edit below this line
