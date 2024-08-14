38ad2aa38bfb656b65a0eccc7cae9c0c4f3d0b61
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-08-14 05:54:09 -0700

    Create fibonacci array and output member of series
    For loop that adds in fibonacci sequence and then goes for as long
    as needed by what the user called for the member. Convert string to
    number in beginning and start the array with two one's to get the loop
    to work correctly. Output various things based on tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arrayOfNums = [];
+    let newNum = parseInt(num, 10);
+    arrayOfNums[0] = 1;
+    arrayOfNums[1] = 1;
+    for (i = 2; i < newNum + 1; i++) {
+        arrayOfNums[i] = arrayOfNums[i-2] + arrayOfNums[i-1];
+    }
+    if (newNum === 0) {
+        return 0;
+    } else if (newNum < 0) {
+        return "OOPS"
+    } else {
+        return arrayOfNums[newNum-1];
+    }
+        
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
