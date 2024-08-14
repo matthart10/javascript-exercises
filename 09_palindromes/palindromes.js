fc7e6adc4c803949c419be28e5bb7c8095240244
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-08-13 18:35:53 -0700

    Create function that checks if palindromes
    Define a function and have the input string remove all punctuation
    and then set everything to lowercase. Define the index to compare
    in the if statement. Then loop through the entire string length, if
    they equal, then keep running through loop, if not, then return
    false immediately.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
 
+const palindromes = function (inputString) {
+let newStringWithCase = inputString.replace(/[^a-zA-Z0-9]/g, '');
+let newString = newStringWithCase.toLowerCase();
+let newStringLengthIndex = newString.length - 1;
+// console.log(newString);
+for (i = 0; i < newString.length; i++) {
+    // console.log(newString[i])
+    if (newString[i] === newString[newStringLengthIndex]) {
+        newStringLengthIndex = newStringLengthIndex - 1;
+    } else {
+        return false;
+    };
+    };
+    return true;
 };
 
+// console.log(palindromes('r3ace3car'));
 // Do not edit below this line
 module.exports = palindromes;
